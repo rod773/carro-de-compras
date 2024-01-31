614ec957341319293a28f65b8134c2a6ea5341e8
rod773 <104315743+rod773@users.noreply.github.com>
2024-01-31 18:54:12 +0100

    class App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div>
-      <h1>Carro De Compras</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Carro De Compras</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
