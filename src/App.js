5219450ac420b46bd9e8a99a9fcf2066fd35a022
rod773 <104315743+rod773@users.noreply.github.com>
2024-01-31 19:04:00 +0100

    productos

--- src/App.js
+++ src/App.js
@@ -2,6 +2,26 @@ import "./App.css";
 import { Component } from "react";
 
 class App extends Component {
+  state = {
+    peoductos: [
+      {
+        name: "Tomate",
+        price: 1500,
+        img: "/productos/tomate.jpg",
+      },
+      {
+        name: "Arbejas",
+        price: 2500,
+        img: "/productos/arbejas.jpg",
+      },
+      {
+        name: "Lechuga",
+        price: 500,
+        img: "/productos/lechuga.jpg",
+      },
+    ],
+  };
+
   render() {
     return (
       <div>
