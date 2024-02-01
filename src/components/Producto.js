1e3abe225513fb7e1f833cbddba4b4dc624bc9e8
rod773 <104315743+rod773@users.noreply.github.com>
2024-02-01 12:36:09 +0100

    button component

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -1,13 +1,30 @@
 import React, { Component } from "react";
+import { Button } from "./Button";
+
+const styles = {
+  producto: {
+    border: "solid 1px #eee",
+    boxShadow: "0 5px 5px rgb(0,0,0,0.1)",
+    width: "30%",
+    padding: "10px 15px",
+    borderRadius: "15px",
+  },
+  img: {
+    width: "100%",
+  },
+};
 
 export class Producto extends Component {
   render() {
-    const { producto } = this.props;
+    const { producto, agregarAlCarro } = this.props;
     return (
-      <div>
-        <img src={producto.img} alt={producto.name} />
+      <div style={styles.producto}>
+        <img style={styles.img} src={producto.img} alt={producto.name} />
         <h3>{producto.name}</h3>
         <p>{producto.price}</p>
+        <Button onClick={() => agregarAlCarro(producto)}>
+          Agregar al carro
+        </Button>
       </div>
     );
   }
